dd025ccfb404b972c151d4da0c74c0e857008913
Daniils <daniils.strals@gmail.com>
2026-04-01 11:50:28 +0300

    Add: added lombok and finalize Product class

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,16 +1,36 @@
 package lv.venta.model;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Product {
 	//1.mainigie
+	@Setter(value = AccessLevel.NONE)
 	private long id;
 	private String title;
 	private float price;
 	private int quantity;
 	private String description;
 	private ProductType productType;
-	//2.getters
-	//3.setters
-	//4.abi konstruktori
-	//5. toString
+	private static long counter = 0;
+	//2.getters - nak no lombok bibliotekas
+	//3.setters - nak no lombok bibliotekas
+	//4.abi konstruktori - bez argumenta konstruktors nak no lombok bibliotekas
+	public Product(String inputTitle, float inputPrice, int inputQuantity, String inputDescription, ProductType inputProductType) {
+		setTitle(inputTitle);
+		setDescription(inputDescription);
+		setPrice(inputPrice);
+		setQuantity(inputQuantity);
+		setProductType(inputProductType);
+		id = counter++;
+	}
+	//5. toString - nak no lombok bibliotekas
 	//6. parejas funkcijas
 }
