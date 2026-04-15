759ee9553dee91b316b25d8bbce80379307f48db
Daniils Dominiks Štrāls <s24stradani@vea.lv>
2026-04-15 09:13:18 +0300

    Add: added ProductCRUDController and getAllProducts method

---
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -0,0 +1,27 @@
+package lv.venta.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lv.venta.service.IProductCRUDService;
+
+@Controller
+@RequestMapping("/product/crud")
+public class ProductCRUDController {
+
+	@Autowired
+	private IProductCRUDService prodService;
+	
+	@GetMapping("/all") //loclahost:8080/product/crud/all
+	public String getAllProducts(Model model) {
+		try {
+			model.addAttribute("package", prodService.retrieveAll());
+			return "show-all-products-page"; //tiks paradita show-all-products-page.html lapa
+		} catch(Exception e) {
+			return "error-page";
+		}
+	}
+}
